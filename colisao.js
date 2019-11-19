410492b3b4f6b6f99dcf894750fc10a5c7d2c8de
matheuscamarques <37029621+matheuscamarques@users.noreply.github.com>
2019-11-19 18:43:39 +0000

    Finalmente
    6 mêses nisso Bobba.

--- colisao.js
+++ colisao.js
@@ -1,26 +1,37 @@
+/*
 
 
-var Bola = [];
+    Autor: Matheus de Camargo Marques
+    
+    Engenharia da Computação - UTFPR 
+
+    
+
+
+*/
+
 var quantidade = 100;
-
+var Bola = [];
+var tam;
 
 function setup() {
-  createCanvas(400, 400);
+  
+  createCanvas(700, 700);
   for(i=0;i<quantidade;i++)
   {
-    var tam = random(20);
+    tam = 20;
     Bola[i] = new Ball(random(width-tam,tam),
                        random(height-tam,tam),
-                       random(-5,5),
-                       random(-5,5),
+                       random(-2,2),
+                       random(-2,2),
                        tam
                       );
-  }  
+   }  
   
 }
 
 function draw() {
-  background(220);
+  background(0);
   
  
  for(i=0;i<quantidade;i++)
@@ -28,7 +39,55 @@ function draw() {
     Bola[i].desenha();
     Bola[i].wallcollide();
   }  
+  
+  colisao();
 
+  
+}
+
+function colisao(){
+
+  var distancia_centros;
+  var x,y;
+
+  
+  for(i=0;i<quantidade;i++){
+    for(j=i+1;j<quantidade;j++)
+      {
+          x = Bola[i].posx - Bola[j].posx ;
+          y = Bola[i].posy - Bola[j].posy ;
+          distancia_centros = x*x + y*y ;
+         // alert(distancia_centros);
+          
+          if(distancia_centros <= tam/2*tam/2*4)
+           {
+              //Atualiza vel
+              var colisao = distancia_centros;
+              
+              var pvx1 = ((Bola[i].velx * x) + (Bola[i].vely * y)) * x/colisao;
+              var pvy1 = ((Bola[i].velx * x) + (Bola[i].vely * y)) * y/colisao;
+              var pvx2 = ((Bola[j].velx * x) + (Bola[j].vely * y)) * x/colisao;
+              var pvy2 = ((Bola[j].velx * x) + (Bola[j].vely * y)) * y/colisao;
+            
+              Bola[i].velx -= (pvx1 - pvx2);
+              Bola[i].vely -= (pvy1 - pvy2);
+              
+              Bola[j].velx -= (pvx2 - pvx1);
+              Bola[j].vely -= (pvy2 - pvy1);
+              
+              if(x != 0 && y !=0){
+                  
+                Bola[i].posx += x / Math.abs(x);
+                Bola[i].posy += y / Math.abs(y);
+              
+                Bola[j].posx -= x / Math.abs(x);
+                Bola[j].posy -= y / Math.abs(y);
+                
+              }
+           }
+          
+      } 
+  }
 }
 
 function Ball(posx,posy,velx,vely,tam){
@@ -42,11 +101,16 @@ function Ball(posx,posy,velx,vely,tam){
   
   this.tam = tam;
   
+  this.R = random(255);
+  this.G = random(255);
+  this.B = random(255);
+  
   this.desenha =  function() {
     
     this.posx= this.posx + this.velx;
     this.posy= this.posy + this.vely ;
     
+    fill(this.R,this.G,this.B);
     ellipse(this.posx,this.posy,this.tam);
     
   }
@@ -66,14 +130,7 @@ function Ball(posx,posy,velx,vely,tam){
     
   }
   
-  this.colisoes = function(){
-    
-    for(i=0;i<Ball.length;i++)
-      for(j=i+1; j<Ball.length;j++){
-            
-            
-      }
-  }
+  
  
   
 }
